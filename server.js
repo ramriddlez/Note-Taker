64fe99dae1c69b450bd50c421062d80504f55495
ramriddlez <ramankangz@outlook.com>
2022-02-22 00:51:52 -0500

    added get functions on server.js

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const path = require('path');
 const fs = require('fs');
 const uuid = require('./helpers/uuid');
 
+// load express into memory 
 const PORT = process.env.PORT || 3001
 
 const app = express();
@@ -12,3 +13,13 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use(express.static('public'));
+
+// define routes
+
+app.get('/', (req,res) =>
+    res.sendFile(path.join(__dirname, '/public/index.html'))
+);
+
+app.get('/notes', (req, res) =>
+    res.sendFile(path.join(__dirname, '/public/notes.html'))
+);
